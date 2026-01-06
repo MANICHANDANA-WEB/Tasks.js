232102f3e53707f11a6d6bda3455c7ca34aaa44f
Mani Chandana <mgudivad@gitam.in>
2026-01-06 22:18:52 +0530

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,7 @@
+let name = "Mani Chandana";   
+let age = 20;                
+const isStudent = true;     
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
